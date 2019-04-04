aac53d6a8968795ee3918d9352637c9e5c3663a9
Pattarawut <pattarawut@hot-now.com>
2019-04-04 17:07:55 +0700

    Fixed space delimiter

--- tests/test_csj.py
+++ tests/test_csj.py
@@ -64,11 +64,29 @@ class TestConvertCsjToDict():
             }
         ]
         result_dict = Csj.to_dict(csj_string)
+        print(result_dict)
         assert result_dict == list_of_dicts
 
     def test_convert_csj_to_dict_with_no_row(self):
         csj_string = '"key1","key2","key3","key4"\n'
         list_of_dicts = []
+        result_dict = Csj.to_dict(csj_string)
+        assert result_dict == list_of_dicts
+
+    def test_convert_csj_to_dict_on_null(self):
+        csj_string = '''"slug","title","released","length_minutes","created","tags","watched_last","watched_times"\n"t2","Terminator2: JudgementDay","1991-07-01",94,"2016-06-1108:54:12.895416+00",["adventure","action","dystopian","robots","time-travel","sci-fi"],null,null\n'''.format('{', '}', '{', '}')
+        list_of_dicts = [
+            {
+                "slug": "t2",
+                "title": "Terminator2: JudgementDay",
+                "released": "1991-07-01",
+                "length_minutes": 94,
+                "created": "2016-06-1108:54:12.895416+00",
+                "tags": ["adventure","action","dystopian","robots","time-travel","sci-fi"],
+                "watched_last": None,
+                "watched_times": None
+            }
+        ]
         result_dict = Csj.to_dict(csj_string)
         assert result_dict == list_of_dicts
 
@@ -80,7 +98,7 @@ class TestConvertDictToCsj():
             {
                 "key1": "value1",
                 "key2": "value2",
-                "key3": "value3",
+                "key3": "null",
                 "key4": "value4"
             },
             {
@@ -90,8 +108,9 @@ class TestConvertDictToCsj():
                 "key4": "value4"
             }
         ]
-        csj_string = '"key1","key2","key3","key4"\n"value1","value2","value3","value4"\n"value1","value2","value3","value4"\n'
+        csj_string = '"key1","key2","key3","key4"\n"value1","value2","null","value4"\n"value1","value2","value3","value4"\n'
         result_csj_string = Csj.from_dicts(list_of_dicts)
+        print(result_csj_string)
         assert result_csj_string == csj_string
     
     def test_convert_dict_to_csj_multiple_row_and_contain_object(self):
@@ -110,5 +129,23 @@ class TestConvertDictToCsj():
             }
         ]
         csj_string = '''"key1","key2","key3","key4"\n"value1",{}"key":"value"{},"value3",{}"key":"value"{}\n"value1","value2","value3","value4"\n'''.format('{', '}', '{', '}')
+        result_csj_string = Csj.from_dicts(list_of_dicts)
+        print(result_csj_string)
+        assert result_csj_string == csj_string
+
+    def test_convert_dict_to_csj_on_null(self):
+        list_of_dicts = [
+            {
+                "slug": "t2",
+                "title": "Terminator 2: Judgement Day",
+                "released": "1991-07-01",
+                "length_minutes": 94,
+                "created": "2016-06-11 08:54:12.895416+00",
+                "tags": ["adventure","action","dystopian","robots","time-travel","sci-fi"],
+                "watched_last": None,
+                "watched_times": None
+            }
+        ]
+        csj_string = '''"slug","title","released","length_minutes","created","tags","watched_last","watched_times"\n"t2","Terminator 2: Judgement Day","1991-07-01",94,"2016-06-11 08:54:12.895416+00",["adventure","action","dystopian","robots","time-travel","sci-fi"],null,null\n'''.format('{', '}', '{', '}')
         result_csj_string = Csj.from_dicts(list_of_dicts)
         assert result_csj_string == csj_string
