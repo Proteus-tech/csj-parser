df43ff445108da6ab9e6b3e97c4d91272d8d75b9
Phanuwat <phanuwat@hot-now.com>
2019-04-04 21:27:52 +0700

    Change to use README.md for Pypi description

--- setup.py
+++ setup.py
@@ -6,12 +6,8 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
-
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
 
 setup(
     name="csj_parser",
@@ -23,8 +19,8 @@ setup(
     author_email="admin@hot-now.com",
 
     description="CSJ for Python",
-    long_description=read("README.md"),
-    long_description_content_type="text/x-rst",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
     packages=find_packages(exclude=('tests',)),
 
