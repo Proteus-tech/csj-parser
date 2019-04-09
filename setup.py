72890a6ae1a3337ddf6dfa736f5c3407b0bb00d6
Phanuwat <phanuwat@hot-now.com>
2019-04-09 20:18:21 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name="csj_parser",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/Proteus-tech/csj-parser",
     license='MIT',
 
